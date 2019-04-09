b0485b78cc9d59a66110fc528b10e2e31d2ea9c4
Radek Ostrowski <radek@startonchain.com>
2019-04-09 14:42:12 +1000

    updates

--- docs/dapp.js
+++ docs/dapp.js
@@ -14,10 +14,7 @@ DApp = {
     walletContract: null,
     walletAbi: [{"constant":true,"inputs":[],"name":"threshold","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"destinations","type":"address[]"},{"name":"values","type":"uint256[]"},{"name":"datas","type":"bytes[]"}],"name":"multicall","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"keyholders","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"destination","type":"address"},{"name":"value","type":"uint256"},{"name":"data","type":"bytes"},{"name":"nonce","type":"uint256"},{"name":"sigs","type":"bytes[]"}],"name":"submit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_threshold","type":"uint256"}],"name":"setThreshold","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"destination","type":"address"},{"name":"value","type":"uint256"},{"name":"data","type":"bytes"},{"name":"nonce","type":"uint256"}],"name":"getTransactionHash","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"nextNonce","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"keyholder","type":"address"},{"name":"weight","type":"uint256"}],"name":"setKeyholderWeight","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"addresses","type":"address[]"},{"name":"weights","type":"uint256[]"},{"name":"_threshold","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"keyholder","type":"address"},{"indexed":false,"name":"weight","type":"uint256"}],"name":"KeyholderChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"threshold","type":"uint256"}],"name":"ThresholdChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"destination","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"},{"indexed":false,"name":"nonce","type":"uint256"},{"indexed":false,"name":"returndata","type":"bytes"},{"indexed":false,"name":"signatories","type":"address[]"}],"name":"Transaction","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Deposit","type":"event"}],
     emptyAddress: '0x0000000000000000000000000000000000000000',
-
     walletAddress: "0x44F5027aAACd75aB89b40411FB119f8Ca82fE733",
-
-//{"contract":"0x44F5027aAACd75aB89b40411FB119f8Ca82fE733", "nonce":0, "users":{"0xa303ddC620aa7d1390BACcc8A495508B183fab59":1,"0x3Abf4443F1Fd1Cc89fc129B44e71dd9c96e260aB":1}, "threshold":1}
 
     init: function() {
         console.log('[x] Initializing DApp.');
@@ -71,47 +68,32 @@ DApp = {
                 }
             });
 
-        // {
-        //     "nonces": {
-        //     "0": {
-        //         "signatures": [
-        //             {
-        //                 "user": "0x02",
-        //                 "msg": "something else"
-        //             }
-        //         ],
-        //             "thresholdReached": false
-        //     },
-        //     "1": {
-        //         "signatures": [
-        //             {
-        //                 "user": "0x02",
-        //                 "msg": "something else"
-        //             }
-        //         ],
-        //             "thresholdReached": false
-        //     }
-        // }
-        // }
-
-        fetch('http://localhost:8080/api/contracts/0x44F5027aAACd75aB89b40411FB119f8Ca82fE733/signatures')
+        fetch('http://localhost:8080/api/contracts/0x44F5027aAACd75aB89b40411FB119f8Ca82fE733/txs')
             .then(function(response) {
                 return response.json();
             })
             .then(function(myJson) {
-                DApp.transactions = myJson.nonces;
+                DApp.transactions = myJson;
                 console.log("XXXXX", JSON.stringify(myJson));
+                //
+                // XXXXX {"1667658391":{"signatures":["signature1","signature2"],
+                //     "txId":1667658391,"tx":{"nonce":2,"destination":"0x3Abf4443F1Fd1Cc89fc129B44e71dd9c96e260aB","data":"0x0","value":"0x0"},
+                //     "thresholdReached":false}}
 
-                let nonces = Object.keys(myJson.nonces);
-                for(let i in nonces){
+
+                let txs = Object.keys(myJson);
+                for(let i in txs){
                     //Nonce	Destination	Value	Current Weights Confirmed	Threshold Reached
-                    console.log("zzz", nonces[i], myJson.nonces[nonces[i]].signatures.length, myJson.nonces[nonces[i]].thresholdReached);
-                    $('#transactions-table').append('<tr><td>' + nonces[i] + '</td>'
-                        + '<td>' + 'destination' + '</td>'
-                        + '<td>' + 'value' + '</td>'
-                        + '<td>' + myJson.nonces[nonces[i]].signatures.length + '</td>'
-                        + '<td>' + myJson.nonces[nonces[i]].thresholdReached + '</td>'
-                        + '<td>' + '<input type="button" class="sign-button" data-txid="' + nonces[i] + '" value="Sign">' + '</td></tr>');
+                    console.log("zzz", txs[i], myJson[txs[i]].tx.nonce, myJson[txs[i]].signatures.length, myJson[txs[i]].thresholdReached);
+                    console.log("nonce",myJson[txs[i]].tx.nonce);
+                    $('#transactions-table').append(
+                        '<tr><td>' + myJson[txs[i]].tx.nonce + '</td>'
+                        + '<td>' + myJson[txs[i]].tx.destination + '</td>'
+                        + '<td>' + myJson[txs[i]].tx.value + '</td>'
+                        + '<td>' + myJson[txs[i]].tx.data + '</td>'
+                        + '<td>' + myJson[txs[i]].signatures.length + '</td>'
+                        + '<td>' + myJson[txs[i]].thresholdReached + '</td>'
+                        + '<td>' + '<input type="button" class="sign-button" data-txid="' + txs[i] + '" value="Sign">' + '</td></tr>');
                 }
             });
     },
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var cors = require('cors')
+let express    = require('express');        // call express
+let app        = express();                 // define our app using express
+let bodyParser = require('body-parser');
+let cors = require('cors');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-var port = process.env.PORT || 8080;        // set our port
+let port = process.env.PORT || 8080;        // set our port
+
+let hashCode = function(s){
+    return JSON.stringify(s).split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+};
 
 //dict of users to pending signatures array
 let contracts = {};
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+let router = express.Router();              // get an instance of the express Router
 
 router.get('/contracts', function(req, res) {
     res.json({ contracts: Object.keys(contracts) });
@@ -34,68 +36,90 @@ router.post('/contracts', function(req, res) {
     res.json({ msg: "added", contract: contracts[req.body.contract]});
 });
 
-//first signature
-//req.body.
-router.post('/contracts/:contract/signatures', function(req, res) {
-    if(!contracts[req.params.contract].nonces) {
-        contracts[req.params.contract].nonces = {}
+
+router.get('/contracts/:contract/nextNonce', function(req, res) {
+    if(!contracts[req.params.contract].txs) {
+        res.json({nonce: contracts[req.params.contract].nonce});
+    } else {
+        let txIds = Object.keys(contracts[req.params.contract].txs);
+        let maxNonce = contracts[req.params.contract].nonce;
+        for(let i in txIds){
+            let txId = txIds[i];
+            let nonce = contracts[req.params.contract].txs[txId].tx.nonce;
+            if(nonce > maxNonce) maxNonce = nonce;
+        }
+        res.json({ nonce: maxNonce+1});
+    }
+});
+
+// {
+//     txId,
+//     tx {nonce destination data value},
+//     signatures []
+// }
+router.post('/contracts/:contract/txs', function(req, res) {
+    if(!contracts[req.params.contract].txs) {
+        contracts[req.params.contract].txs = {}
     }
 
-    //requires user to be one of the signatories
-    //user and their signature
-    let nonce = contracts[req.params.contract].nonce;
+    let tx = req.body.tx;
+    let txId = hashCode(tx);
+    let signature = req.body.signature;
 
-    contracts[req.params.contract].nonces[nonce] = {"signatures":[req.body]};
+    if(!contracts[req.params.contract].txs[txId]){
+        //create
+        contracts[req.params.contract].txs[txId] = {signatures:[signature], txId:txId, tx:tx};
+    } else {
+        //append signature
+        contracts[req.params.contract].txs[txId].signatures.push(signature);
+    }
 
     let sum = 0;
-    for (let sig in contracts[req.params.contract].nonces[nonce].signatures) {
-        let user = contracts[req.params.contract].nonces[nonce].signatures[sig].user;
+    for (let sig in contracts[req.params.contract].txs[txId].signatures) {
+        //ecrecover user
+        let user = contracts[req.params.contract].txs[txId].signatures[sig].user;
         sum += contracts[req.params.contract].users[user];
     }
 
     if(sum >= contracts[req.params.contract].threshold) {
-        contracts[req.params.contract].nonces[nonce].thresholdReached = true;
+        contracts[req.params.contract].txs[txId].thresholdReached = true;
     } else {
-        contracts[req.params.contract].nonces[nonce].thresholdReached = false;
+        contracts[req.params.contract].txs[txId].thresholdReached = false;
     }
 
-    contracts[req.params.contract].nonce += 1;
-    res.json({ nonce: nonce, details: contracts[req.params.contract].nonces[nonce] });
+    res.json({ txId: txId, details: contracts[req.params.contract].txs[txId] });
 });
 
-router.get('/contracts/:contract/signatures', function(req, res) {
-    if(contracts[req.params.contract].nonce) {
-        res.json({ nonces: contracts[req.params.contract].nonces});
+router.get('/contracts/:contract/txs', function(req, res) {
+    if(contracts[req.params.contract].txs) {
+        res.json(contracts[req.params.contract].txs);
     } else {
-        res.json( { nonces: {}});
+        res.json( { txs: {}});
     }
 });
 
-router.get('/contracts/:contract/signatures/:nonce', function(req, res) {
-    //return the array of signed messages
-    res.json(contracts[req.params.contract].nonces[req.params.nonce]);
-});
-
-router.post('/contracts/:contract/signatures/:nonce', function(req, res) {
-    //return the array of signed messages
-
-    contracts[req.params.contract].nonces[req.params.nonce].signatures.push(req.body);
-
-    let sum = 0;
-    for (let sig in contracts[req.params.contract].nonces[req.params.nonce].signatures) {
-        let user = contracts[req.params.contract].nonces[req.params.nonce].signatures[sig].user;
-        sum += contracts[req.params.contract].users[user];
-    }
-    console.log("past - sum",sum,"threshold",contracts[req.params.contract].threshold);
-
-    if(sum >= contracts[req.params.contract].threshold) {
-        contracts[req.params.contract].nonces[req.params.nonce].thresholdReached = true;
-    } else {
-        contracts[req.params.contract].nonces[req.params.nonce].thresholdReached = false;
-    }
-
-    res.json(contracts[req.params.contract].nonces[req.params.nonce]);
-});
+// router.get('/contracts/:contract/txs/:txId', function(req, res) {
+//     res.json(contracts[req.params.contract].txs[req.params.txId]);
+// });
+//
+// router.post('/contracts/:contract/txs/:txId', function(req, res) {
+//     contracts[req.params.contract].txs[req.params.txId].signatures.push(req.body);
+//
+//     let sum = 0;
+//     for (let sig in contracts[req.params.contract].txs[req.params.txId].signatures) {
+//         let user = contracts[req.params.contract].txs[req.params.txId].signatures[sig].user;
+//         sum += contracts[req.params.contract].users[user];
+//     }
+//     console.log("past - sum",sum,"threshold",contracts[req.params.contract].threshold);
+//
+//     if(sum >= contracts[req.params.contract].threshold) {
+//         contracts[req.params.contract].txs[req.params.txId].thresholdReached = true;
+//     } else {
+//         contracts[req.params.contract].txs[req.params.txId].thresholdReached = false;
+//     }
+//
+//     res.json(contracts[req.params.contract].txs[req.params.txId]);
+// });
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
